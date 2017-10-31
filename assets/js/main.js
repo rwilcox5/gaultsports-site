f01ff2a0fdb7bad2198b88eef8cfca6067a4d0e9
rwilcox5 <rwilcox5@gmail.com>
2017-10-31 19:26:44 -0400

    nm

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction(elementID) {
+    document.getElementById(elementID).classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropnfl')) {
+    var nflDropdown = document.getElementById("nflDropdown");
+      if (nflDropdown.classList.contains('show')) {
+        nflDropdown.classList.remove('show');
+      }
+  }
+  if (!e.target.matches('.dropncaaf')) {
+    var ncaafDropdown = document.getElementById("ncaafDropdown");
+      if (ncaafDropdown.classList.contains('show')) {
+        ncaafDropdown.classList.remove('show');
+      }
+  }
+  if (!e.target.matches('.dropmlb')) {
+    var mlbDropdown = document.getElementById("mlbDropdown");
+      if (mlbDropdown.classList.contains('show')) {
+        mlbDropdown.classList.remove('show');
+      }
+  }
+  if (!e.target.matches('.dropolympic')) {
+    var olympicDropdown = document.getElementById("olympicDropdown");
+      if (olympicDropdown.classList.contains('show')) {
+        olympicDropdown.classList.remove('show');
+      }
+  }
+}
+
+
